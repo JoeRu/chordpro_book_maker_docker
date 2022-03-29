d93edd14468be6b8f7606ada256db42345d1ee37
Johannes Rumpf <johannes.rumpf@gmail.com>
2022-03-29 10:57:35 +0200

    update

--- build_book.bat
+++ build_book.bat
@@ -1,5 +1,5 @@
-docker-compose down --remove-orphans
-docker-compose build
+@REM docker-compose down --remove-orphans
+@REM docker-compose build 
 docker-compose run chordpro make_chordpro_book.sh
 docker-compose run latex make_tex_book.sh
 docker-compose down --remove-orphans
--- latex/make_tex_book.sh
+++ latex/make_tex_book.sh
@@ -4,7 +4,10 @@ echo "Parameter für default Filename: $INPUTP(.tex, .md)"
 
 cd /data/output
 lualatex $INPUTP.tex
+# texlua songidx.lua -b bible.can scrpfile.sxd scrpfile.sbx
+texlua songidx.lua -b protestant.can cbauth.sxd cbauth.sbx
 texlua songidx.lua -b protestant.can cbtitle.sxd cbtitle.sbx
+texlua songidx.lua -b protestant.can cbscrip.sxd cbscrip.sbx
 lualatex $INPUTP.tex
 #pandoc $INPUTP.md  metadata.txt --metadata title="Stamm Hattstein Lieder"  --table-of-contents -s -o $INPUTP.html
 #pandoc --toc --toc-depth=1 --epub-metadata=metadata.txt --epub-cover-image=Stamm-Hattstein-komplett.png --css=liederbuch.css -o $INPUTP.epub metadata.txt $INPUTP.md 
