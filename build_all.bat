2a72f650aca4da7ccc92505a56390229248505f3
Johannes Rumpf <johannes.rumpf@gmail.com>
2022-03-31 15:48:33 +0200

    some more build options

--- build_all.bat
+++ build_all.bat
@@ -1,4 +1,4 @@
 docker-compose down --remove-orphans
 docker-compose run chordpro make_chordpro_book.sh
-docker-compose run latex make_tex_book.sh
+docker-compose run latex make_all.sh
 docker-compose down --remove-orphans
