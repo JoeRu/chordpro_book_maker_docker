fc5fc548faf84f49b39c2cebb414c60a84641019
Johannes Rumpf <johannes.rumpf@gmail.com>
2022-04-12 22:40:48 +0200

    image mit font-update

--- build_complete_book.bat
+++ build_complete_book.bat
@@ -1,5 +1,5 @@
 docker-compose down --remove-orphans
-docker-compose run chordpro /bin/rm -Rf /data/output/
+docker-compose run chordpro clear.sh
 docker-compose run chordpro make_chordpro_book.sh Stammeslieder/*.pro output/Stammeslieder
 docker-compose run chordpro make_chordpro_book.sh Essenslieder/*.pro output/Essenslieder
 docker-compose run chordpro make_chordpro_book.sh Meutenlieder/*.pro output/Meutenlieder
@@ -11,8 +11,7 @@ docker-compose run chordpro make_chordpro_book.sh Pfadfinderlieder/Fahrten/*.pro
 docker-compose run chordpro make_chordpro_book.sh Roverlieder/Kneipen-Lieder/*.pro output/Kneipen-Lieder
 docker-compose run chordpro make_chordpro_book.sh Roverlieder/Landsknecht+Soldatenlieder/*.pro output/Landsknecht+Soldatenlieder
 docker-compose run chordpro make_chordpro_book.sh Roverlieder/Politische_Lieder/*.pro output/PolitischeLieder
-docker-compose run chordpro /bin/cp /data/*.md /data/output/
-docker-compose run chordpro /bin/cp /data/*.png /data/output/
+docker-compose run chordpro prepare.sh
 
 docker-compose run latex make_complete_tex_book.sh
 docker-compose down --remove-orphans
