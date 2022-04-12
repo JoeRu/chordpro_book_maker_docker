09f87e6cbc5d240178802ec3c8f2d81a1e54aa11
Johannes Rumpf <johannes.rumpf@gmail.com>
2022-04-12 21:11:01 +0200

    update

--- build_book.bat
+++ build_book.bat
@@ -1,4 +1,18 @@
 docker-compose down --remove-orphans
-docker-compose run chordpro make_chordpro_book.sh
+docker-compose run chordpro /bin/rm -Rf /data/output/
+docker-compose run chordpro make_chordpro_book.sh Stammeslieder/*.pro output/Stammeslieder
+docker-compose run chordpro make_chordpro_book.sh Essenslieder/*.pro output/Essenslieder
+docker-compose run chordpro make_chordpro_book.sh Meutenlieder/*.pro output/Meutenlieder
+docker-compose run chordpro make_chordpro_book.sh Pfadfinderlieder/Morgenlieder/*.pro output/Morgenlieder
+docker-compose run chordpro make_chordpro_book.sh Pfadfinderlieder/Am_Lagerfeuer/*.pro output/AmLagerfeuer
+docker-compose run chordpro make_chordpro_book.sh Pfadfinderlieder/Abendlieder/*.pro output/Abendlieder
+docker-compose run chordpro make_chordpro_book.sh Pfadfinderlieder/Seemansgarn/*.pro output/Seemansgarn
+docker-compose run chordpro make_chordpro_book.sh Pfadfinderlieder/Fahrten/*.pro output/Fahrten
+docker-compose run chordpro make_chordpro_book.sh Roverlieder/Kneipen-Lieder/*.pro output/Kneipen-Lieder
+docker-compose run chordpro make_chordpro_book.sh Roverlieder/Landsknecht+Soldatenlieder/*.pro output/Landsknecht+Soldatenlieder
+docker-compose run chordpro make_chordpro_book.sh Roverlieder/Politische_Lieder/*.pro output/PolitischeLieder
+docker-compose run chordpro /bin/cp /data/*.md /data/output/
+docker-compose run chordpro /bin/cp /data/*.png /data/output/
+
 docker-compose run latex make_tex_book.sh
 docker-compose down --remove-orphans
--- chordpro/make_chordpro_book.sh
+++ chordpro/make_chordpro_book.sh
@@ -4,16 +4,21 @@ OUTPUTP=${2:-output/liederbuch}
 
 echo "Eingabe-Dateie(n): $INPUTP"
 echo "Ausgabe-Datei: $OUTPUTP.(tex, md)"
+INPUT_PATH=$(echo $INPUTP | sed 's/\(\/\*\.\)\(pro\|cho\)//g')
+echo "Inputpath : $INPUT_PATH"
 
 if ! [[ -d /data/output/ ]]
 then
     mkdir /data/output
 fi
 cd /data
+rm $OUTPUTP.tex
 chordpro --config=build/myconfig.json $INPUTP --output $OUTPUTP.tex
+rm $OUTPUTP.md
 chordpro --config=build/myconfig.json $INPUTP --output $OUTPUTP.md
 
-cp *.png output/
-cp *.md output/
+
+cp $INPUT_PATH/*.png output/
+cp $INPUT_PATH/*.md output/
 #cp *.tex output/
 cp -R build/* output/
