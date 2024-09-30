50c0d5ee006aa24b2f157abd8b7a87ee6dbbfbe4
Johannes Rumpf <johannes.rumpf@gmail.com>
2024-09-30 22:43:40 +0200

    update compose files

--- build_all.bat
+++ build_all.bat
@@ -1,19 +1,19 @@
-docker-compose down --remove-orphans
-docker-compose run chordpro clear.sh
-docker-compose run chordpro make_chordpro_book.sh Stammeslieder/*.pro output/Stammeslieder
-docker-compose run chordpro make_chordpro_book.sh Essenslieder/*.pro output/Essenslieder
-docker-compose run chordpro make_chordpro_book.sh Meutenlieder/*.pro output/Meutenlieder
-docker-compose run chordpro make_chordpro_book.sh Pfadfinderlieder/Morgenlieder/*.pro output/Morgenlieder
-docker-compose run chordpro make_chordpro_book.sh Pfadfinderlieder/Am_Lagerfeuer/*.pro output/AmLagerfeuer
-docker-compose run chordpro make_chordpro_book.sh Pfadfinderlieder/Abendlieder/*.pro output/Abendlieder
-docker-compose run chordpro make_chordpro_book.sh Pfadfinderlieder/Seemansgarn/*.pro output/Seemansgarn
-docker-compose run chordpro make_chordpro_book.sh Pfadfinderlieder/Fahrten/*.pro output/Fahrten
-docker-compose run chordpro make_chordpro_book.sh Roverlieder/Kneipen-Lieder/*.pro output/Kneipen-Lieder
-docker-compose run chordpro make_chordpro_book.sh Roverlieder/Landsknecht+Soldatenlieder/*.pro output/Landsknecht+Soldatenlieder
-docker-compose run chordpro make_chordpro_book.sh Roverlieder/Politische_Lieder/*.pro output/PolitischeLieder
-docker-compose run chordpro prepare.sh
+docker compose down --remove-orphans
+docker compose run chordpro clear.sh
+docker compose run chordpro make_chordpro_book.sh Stammeslieder/*.pro output/Stammeslieder
+docker compose run chordpro make_chordpro_book.sh Essenslieder/*.pro output/Essenslieder
+docker compose run chordpro make_chordpro_book.sh Meutenlieder/*.pro output/Meutenlieder
+docker compose run chordpro make_chordpro_book.sh Pfadfinderlieder/Morgenlieder/*.pro output/Morgenlieder
+docker compose run chordpro make_chordpro_book.sh Pfadfinderlieder/Am_Lagerfeuer/*.pro output/AmLagerfeuer
+docker compose run chordpro make_chordpro_book.sh Pfadfinderlieder/Abendlieder/*.pro output/Abendlieder
+docker compose run chordpro make_chordpro_book.sh Pfadfinderlieder/Seemansgarn/*.pro output/Seemansgarn
+docker compose run chordpro make_chordpro_book.sh Pfadfinderlieder/Fahrten/*.pro output/Fahrten
+docker compose run chordpro make_chordpro_book.sh Roverlieder/Kneipen-Lieder/*.pro output/Kneipen-Lieder
+docker compose run chordpro make_chordpro_book.sh Roverlieder/Landsknecht+Soldatenlieder/*.pro output/Landsknecht+Soldatenlieder
+docker compose run chordpro make_chordpro_book.sh Roverlieder/Politische_Lieder/*.pro output/PolitischeLieder
+docker compose run chordpro prepare.sh
 
-docker-compose run latex make_all.sh
-docker-compose run latex make_ebook.sh
+docker compose run latex make_all.sh
+docker compose run latex make_ebook.sh
 powershell convert_ebook2kindle.ps1
-docker-compose down --remove-orphans
+docker compose down --remove-orphans
--- build_ebook.bat
+++ build_ebook.bat
@@ -1,18 +1,18 @@
-docker-compose down --remove-orphans
+docker compose down --remove-orphans
 Powershell.exe -File clean_output.ps1
 
-docker-compose run chordpro make_chordpro_book.sh Stammeslieder/*.pro output/Stammeslieder
-docker-compose run chordpro make_chordpro_book.sh Essenslieder/*.pro output/Essenslieder
-docker-compose run chordpro make_chordpro_book.sh Meutenlieder/*.pro output/Meutenlieder
-docker-compose run chordpro make_chordpro_book.sh Pfadfinderlieder/Morgenlieder/*.pro output/Morgenlieder
-docker-compose run chordpro make_chordpro_book.sh Pfadfinderlieder/Am_Lagerfeuer/*.pro output/AmLagerfeuer
-docker-compose run chordpro make_chordpro_book.sh Pfadfinderlieder/Abendlieder/*.pro output/Abendlieder
-docker-compose run chordpro make_chordpro_book.sh Pfadfinderlieder/Seemansgarn/*.pro output/Seemansgarn
-docker-compose run chordpro make_chordpro_book.sh Pfadfinderlieder/Fahrten/*.pro output/Fahrten
-docker-compose run chordpro make_chordpro_book.sh Roverlieder/Kneipen-Lieder/*.pro output/Kneipen-Lieder
-docker-compose run chordpro make_chordpro_book.sh Roverlieder/Landsknecht+Soldatenlieder/*.pro output/Landsknecht+Soldatenlieder
-docker-compose run chordpro make_chordpro_book.sh Roverlieder/Politische_Lieder/*.pro output/PolitischeLieder
+docker compose run chordpro make_chordpro_book.sh Stammeslieder/*.pro output/Stammeslieder
+docker compose run chordpro make_chordpro_book.sh Essenslieder/*.pro output/Essenslieder
+docker compose run chordpro make_chordpro_book.sh Meutenlieder/*.pro output/Meutenlieder
+docker compose run chordpro make_chordpro_book.sh Pfadfinderlieder/Morgenlieder/*.pro output/Morgenlieder
+docker compose run chordpro make_chordpro_book.sh Pfadfinderlieder/Am_Lagerfeuer/*.pro output/AmLagerfeuer
+docker compose run chordpro make_chordpro_book.sh Pfadfinderlieder/Abendlieder/*.pro output/Abendlieder
+docker compose run chordpro make_chordpro_book.sh Pfadfinderlieder/Seemansgarn/*.pro output/Seemansgarn
+docker compose run chordpro make_chordpro_book.sh Pfadfinderlieder/Fahrten/*.pro output/Fahrten
+docker compose run chordpro make_chordpro_book.sh Roverlieder/Kneipen-Lieder/*.pro output/Kneipen-Lieder
+docker compose run chordpro make_chordpro_book.sh Roverlieder/Landsknecht+Soldatenlieder/*.pro output/Landsknecht+Soldatenlieder
+docker compose run chordpro make_chordpro_book.sh Roverlieder/Politische_Lieder/*.pro output/PolitischeLieder
 
-docker-compose run latex make_ebook.sh
+docker compose run latex make_ebook.sh
 Powershell.exe -File convert_ebook2kindle.ps1
-docker-compose down --remove-orphans
+docker compose down --remove-orphans
